809bd53b0554ebcaada7943b4787b92d03ec698c
PRATAP PANABAKA <pratap@fastmail.fm>
2022-11-04 11:18:43 +0530

    Update 04-11-2022.js

--- 04-11-2022.js
+++ 04-11-2022.js
@@ -16,3 +16,10 @@ console.log(longestString(inputArray)) /* => 'sky is blue' */
 
 const inputArray2 = ['hello', 'world', 'abcde', 'efghi']	
 console.log(longestString(inputArray2)) /* => 'hello' */
+
+/*
+[admin@ANTARCTICA daily-js-practice]$ gjs 04-11-2022.js 
+Gjs-Console-Message: 11:16:52.394: sky is blue
+Gjs-Console-Message: 11:16:52.395: hello
+[admin@ANTARCTICA daily-js-practice]$
+*/
