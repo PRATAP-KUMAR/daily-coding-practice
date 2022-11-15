f515325d3ee804f6f6998b37dde957fc29ed5744
PRATAP-KUMAR <pratap@fastmail.fm>
2022-11-15 12:58:04 +0530

    mob-programming calculate multiplication without multiplying

--- 13-11-2022.rb
+++ 13-11-2022.rb
@@ -10,16 +10,17 @@ In this challenge, write a function in Ruby that takes two parameters, both of t
 def divideby2(num)
     if(num < 2)
         return 0
-    return 1 + divideby2(num - 2)
+    else
+        return 1 + divideby2(num - 2)
     end
 end
 
 def multi_with_out_into(num1, num2)
     square = (num1 + num2) ** 2
-    num1_square = (num1) ** 2
+    num1_square = ((num1) ** 2)
     num2_square = (num2) ** 2
     
-    val = square - num1_square - num2_square
+    val = (square - num1_square - num2_square)
     
     if(val >= 0)
         ans = divideby2(val)
@@ -29,7 +30,14 @@ def multi_with_out_into(num1, num2)
     return ans
 end
 
-num1 = 5
+num1 = 13
 num2 = -2
 
 puts multi_with_out_into(num1, num2) # => -10
+
+=begin
+[admin@ANTARCTICA daily-js-practice]$ ruby 13-11-2022.rb 
+-26
+=end
+
+
