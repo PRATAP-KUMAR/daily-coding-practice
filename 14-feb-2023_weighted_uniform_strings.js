bf8954385d16ba84299e138e941cba5934d8e19e
PRATAP-KUMAR <pratap@fastmail.fm>
2023-02-14 11:59:57 +0530

    Update code

--- 14-feb-2023_weighted_uniform_strings.js
+++ 14-feb-2023_weighted_uniform_strings.js
@@ -1,16 +1,31 @@
 const myFunc = (s, queries) => {
-    let weight = [];
-    let array = [];
-    for(let i=1; i<[...s].length; i++) {
-        let prvs = [...s][i-1].charCodeAt() - 96;
+    let values = [[...s][0].charCodeAt() - 96];
+    let reserved = 0;
+
+    for (let i = 1; i < [...s].length; i++) {
+        let prvs = [...s][i - 1].charCodeAt() - 96;
         let curr = [...s][i].charCodeAt() - 96;
-        let value = curr === prvs ? curr+prvs : curr;
-        console.log(prvs, value)
+        if (curr === prvs) {
+            if (reserved === 0) {
+                reserved += curr + prvs;
+            } else {
+                reserved += curr;
+            }
+            values.push(reserved);
+        } else {
+            reserved = 0;
+            values.push(curr);
+        }
     }
-    return 'completed'
+    let result = [];
+    const set = new Set(values);
+    queries.forEach((num) => {
+        result.push(set.has(num) ? 'Yes' : 'No');
+    });
+    return result;
 }
 
-const s = 'abccdde';
-const queries = [1, 3, 12, 5, 9, 10]
+const s = 'abbcccdddd';
+const queries = [1, 7, 5, 4, 15]
 
 console.log(myFunc(s, queries));
